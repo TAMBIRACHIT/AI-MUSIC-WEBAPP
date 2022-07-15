a111a60c97188f555d7760eb3ff89ddf64da1d6b
Rachit <95204792+Coder519@users.noreply.github.com>
2022-07-15 17:03:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function preload()
 	song = loadSound("Na Ja - Pav Dharia.mp3");
     song = loadSound("Pasoori - Shae Gill.mp3");
 }
+scoreLeftWrist = 0;
 
 rightWristX = 0;
 rightWristY = 0;
@@ -32,6 +33,9 @@ function gotPoses(results)
   if(results.length > 0)
   {
 	  console.log(results);
+            scoreLeftWrist = results[0].pose.keypoints[9].score;
+	  console.log("scoreLeftWrist =" + scoreLeftWrist);
+
 	  leftWristX = results[0].pose.leftWrist.x;
 	  leftWristY = results[0].pose.leftWrist.y;
 console.log("leftWristX =" + leftWristX +"leftWristY"+leftWristY);  
@@ -45,7 +49,16 @@ console.log("rightWristX =" + rightWristX +"rightWristY"+rightWristY);
 function draw() {
 	image(video, 0, 0, 600, 500);
 
-	
+	fill("#FF0000");
+
+	stroke("#FF0000");
+
+	circle(leftWristX,leftWristY,20);
+	InNumberleftWristY = Number(leftWristY);
+	remove_decimals = floor(InNumberleftWristY);
+	volume = remove_decimals/500 ;
+	document.getElementById("volume").innerHTML = "Volume =" + volume
+	song.setVolume(volume);
 
 	
 
