5d8331c5ef8db3d53af831796221ed911cef9bbd
Rachit tambi <95204792+Coder519@users.noreply.github.com>
2022-07-29 17:37:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ song = "";
 
 function preload()
 {
-	song = loadSound("Na Ja - Pav Dharia.mp3");
-    song = loadSound("Pasoori - Shae Gill.mp3");
+	song = loadSound("Pasoori - Shae Gill.mp3");
 }
+
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 rightWristX = 0;
 rightWristY = 0;
@@ -33,7 +34,7 @@ function gotPoses(results)
   if(results.length > 0)
   {
 	  console.log(results);
-            scoreLeftWrist = results[0].pose.keypoints[9].score;
+	  scoreLeftWrist = results[0].pose.keypoints[9].score;
 	  console.log("scoreLeftWrist =" + scoreLeftWrist);
 
 	  leftWristX = results[0].pose.leftWrist.x;
@@ -52,6 +53,38 @@ function draw() {
 	fill("#FF0000");
 
 	stroke("#FF0000");
+	if(scoreRightWrist > 0.2)
+	{
+
+	circle(rightWristX,rightWristY,20)
+
+	if(rightWristY >0 && rightWristY<= 100)
+	{
+		document.getElementById("speed").innerHTML = 'Speed = 0.5x';
+		song.rate(0.5);
+	}
+	else if(rightWristY >100 && rightWristY<=200)
+	{
+		document.getElementById("speed").innerHTML = 'Speed = 1x';
+		song.rate(1);
+	}
+	else if(rightWristY >200 && rightWristY<=300)
+	{
+		document.getElementById("speed").innerHTML = 'Speed = 1.5x';
+		song.rate(1.5);
+	}
+	else if(rightWristY >300 && rightWristY<=400)
+	{
+		document.getElementById("speed").innerHTML = 'Speed = 2x';
+		song.rate(2);
+	}else if(rightWristY >400 && rightWristY<=500)
+	{
+		document.getElementById("speed").innerHTML = 'Speed = 2.5x';
+		song.rate(2.5);
+	}
+	}
+
+if(scoreLeftWrist >0.2){
 
 	circle(leftWristX,leftWristY,20);
 	InNumberleftWristY = Number(leftWristY);
@@ -60,13 +93,15 @@ function draw() {
 	document.getElementById("volume").innerHTML = "Volume =" + volume
 	song.setVolume(volume);
 
+}
 	
+
+
 
 }
 
 function play()
 {
-	song.play();
-	song.setVolume(1);
+	song.setVolume(1);            
 	song.rate(1);
 }
